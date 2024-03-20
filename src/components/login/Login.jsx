d3aebb03adc5482acec1503f1d39294729614116
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2024-03-20 20:16:00 +0000

    cleaning code

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -11,7 +11,6 @@ import axios from "axios";
 
 const Login = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [loginStep, setLoginStep] = useState(0);
 
   const {
     register,
@@ -30,91 +29,52 @@ const Login = () => {
       <div className="login_form">
         <form
           onSubmit={handleSubmit((data) => {
-            if (loginStep == 0) setLoginStep(1);
-            if (loginStep == 1) {
-              axios
-                .post("http://localhost:8080/auth/login?otpCode=" + data.otp, {
-                  email: data.email,
-                  password: data.password,
-                })
-                .then(function (response) {
-                  console.log(response);
-                })
-                .catch(function (error) {
-                  console.log(error);
-                });
-            }
+            console.log(data);
           })}
         >
-          <header>{loginStep == 0 ? "Sign in" : "Verification Code"}</header>
+          <header>Sign in</header>
 
-          {loginStep == 0 && (
-            <>
-              <div className="form_input_group">
-                <label>Email or phone number</label>
-                <input type="email" {...register("email")} />
-                {errors.email && (
-                  <span className="error">{errors.email.message}</span>
-                )}
-              </div>
-              <div className="form_input_group">
-                <div className="password_metadata">
-                  <label>Password</label>
-                  {isVisible && (
-                    <GoEyeClosed
-                      style={{ cursor: "pointer" }}
-                      onClick={handlePasswordVisibility}
-                    />
-                  )}
-                  {!isVisible && (
-                    <GoEye
-                      style={{ cursor: "pointer" }}
-                      onClick={handlePasswordVisibility}
-                    />
-                  )}
-                </div>
-                <input
-                  type={isVisible ? "text" : "password"}
-                  {...register("password")}
+          <div className="form_input_group">
+            <label>Email or phone number</label>
+            <input type="email" {...register("email")} />
+            {errors.email && (
+              <span className="error">{errors.email.message}</span>
+            )}
+          </div>
+          <div className="form_input_group">
+            <div className="password_metadata">
+              <label>Password</label>
+              {isVisible && (
+                <GoEyeClosed
+                  style={{ cursor: "pointer" }}
+                  onClick={handlePasswordVisibility}
                 />
-                {errors.password && (
-                  <span className="error">{errors.password.message}</span>
-                )}
-              </div>
-              <div className="form_input_group">
-                <label>Select your role: </label>
-                <select {...register("role")} defaultValue="user">
-                  <option value="admin">Admin</option>
-                  <option value="supplier">Supplier</option>
-                  <option value="user">User</option>
-                </select>
-              </div>
-            </>
-          )}
-
-          {loginStep == 1 && (
-            <div className="otp">
-              <Controller
-                name="otp"
-                control={control}
-                render={({ field }) => (
-                  <OTPInput
-                    inputStyle={{ width: 50, height: 50 }}
-                    value={field.value}
-                    onChange={field.onChange}
-                    numInputs={6}
-                    renderSeparator={<span>-</span>}
-                    renderInput={(props) => <input {...props} />}
-                  />
-                )}
-              />
-              {errors.otp && (
-                <span className="error">{errors.otp.message}</span>
+              )}
+              {!isVisible && (
+                <GoEye
+                  style={{ cursor: "pointer" }}
+                  onClick={handlePasswordVisibility}
+                />
               )}
             </div>
-          )}
+            <input
+              type={isVisible ? "text" : "password"}
+              {...register("password")}
+            />
+            {errors.password && (
+              <span className="error">{errors.password.message}</span>
+            )}
+          </div>
+          <div className="form_input_group">
+            <label>Select your role: </label>
+            <select {...register("role")} defaultValue="user">
+              <option value="admin">Admin</option>
+              <option value="supplier">Supplier</option>
+              <option value="user">User</option>
+            </select>
+          </div>
 
-          <button>{loginStep == 0 ? "Next" : "Sign up"}</button>
+          <button>Sign up</button>
           <div className="remember">
             <input type="checkbox" {...register("remember")} />
             <p>Remember me</p>
--- src/components/login/loginValidation.js
+++ src/components/login/loginValidation.js
@@ -4,7 +4,6 @@ const loginSchema = yup
   .object({
     email: yup.string().required("Email required").email("Invalid email"),
     password: yup.string().required("Password required"),
-    otp: yup.string().length(6),
   })
   .required();
 
