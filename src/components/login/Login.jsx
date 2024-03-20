791050e54ef7c02f7c9b542f664eb558d9bd5752
ilyass <ilyass.haddoud@inetum.com>
2024-03-20 11:24:36 +0000

    adding otp verification

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,16 +3,21 @@ import { GoEye } from "react-icons/go";
 import { GoEyeClosed } from "react-icons/go";
 import "./login.css";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import loginSchema from "./loginValidation";
 import { useState } from "react";
+import OTPInput from "react-otp-input";
+import axios from "axios";
 
 const Login = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [loginStep, setLoginStep] = useState(0);
+  const [otp, setOtp] = useState("");
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm({ resolver: yupResolver(loginSchema) });
   const handlePasswordVisibility = () => {
     setIsVisible(!isVisible);
@@ -25,51 +30,91 @@ const Login = () => {
       <div className="login_form">
         <form
           onSubmit={handleSubmit((data) => {
-            console.log(data);
+            if (loginStep == 0) setLoginStep(1);
+            if (loginStep == 1) {
+              axios
+                .post("http://localhost:8080/auth/login?otpCode=" + data.otp, {
+                  email: data.email,
+                  password: data.password,
+                })
+                .then(function (response) {
+                  console.log(response);
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
+            }
           })}
         >
-          <header>Sign in</header>
-          <div className="form_input_group">
-            <label>Email or phone number</label>
-            <input type="email" {...register("email")} />
-            {errors.email && (
-              <span className="error">{errors.email.message}</span>
-            )}
-          </div>
-          <div className="form_input_group">
-            <div className="password_metadata">
-              <label>Password</label>
-              {isVisible && (
-                <GoEyeClosed
-                  style={{ cursor: "pointer" }}
-                  onClick={handlePasswordVisibility}
+          <header>{loginStep == 0 ? "Sign in" : "Verification Code"}</header>
+
+          {loginStep == 0 && (
+            <>
+              <div className="form_input_group">
+                <label>Email or phone number</label>
+                <input type="email" {...register("email")} />
+                {errors.email && (
+                  <span className="error">{errors.email.message}</span>
+                )}
+              </div>
+              <div className="form_input_group">
+                <div className="password_metadata">
+                  <label>Password</label>
+                  {isVisible && (
+                    <GoEyeClosed
+                      style={{ cursor: "pointer" }}
+                      onClick={handlePasswordVisibility}
+                    />
+                  )}
+                  {!isVisible && (
+                    <GoEye
+                      style={{ cursor: "pointer" }}
+                      onClick={handlePasswordVisibility}
+                    />
+                  )}
+                </div>
+                <input
+                  type={isVisible ? "text" : "password"}
+                  {...register("password")}
                 />
-              )}
-              {!isVisible && (
-                <GoEye
-                  style={{ cursor: "pointer" }}
-                  onClick={handlePasswordVisibility}
-                />
+                {errors.password && (
+                  <span className="error">{errors.password.message}</span>
+                )}
+              </div>
+              <div className="form_input_group">
+                <label>Select your role: </label>
+                <select {...register("role")} defaultValue="user">
+                  <option value="admin">Admin</option>
+                  <option value="supplier">Supplier</option>
+                  <option value="user">User</option>
+                </select>
+              </div>
+            </>
+          )}
+
+          {loginStep == 1 && (
+            <div className="otp">
+              <Controller
+                name="otp"
+                control={control}
+                render={({ field }) => (
+                  <OTPInput
+                    inputStyle={{ width: 50, height: 50 }}
+                    value={field.value}
+                    onChange={field.onChange}
+                    numInputs={6}
+                    renderSeparator={<span>-</span>}
+                    renderInput={(props) => <input {...props} />}
+                  />
+                )}
+              />
+              {errors.otp && (
+                <span className="error">{errors.otp.message}</span>
               )}
             </div>
-            <input
-              type={isVisible ? "text" : "password"}
-              {...register("password")}
-            />
-            {errors.password && (
-              <span className="error">{errors.password.message}</span>
-            )}
-          </div>
-          <div className="form_input_group">
-            <label>Select your role: </label>
-            <select {...register("role")} defaultValue="user">
-              <option value="admin">Admin</option>
-              <option value="supplier">Supplier</option>
-              <option value="user">User</option>
-            </select>
-          </div>
+          )}
 
-          <button>Next</button>
+          <button>{loginStep == 0 ? "Next" : "Sign up"}</button>
           <div className="remember">
             <input type="checkbox" {...register("remember")} />
             <p>Remember me</p>
--- src/components/login/loginValidation.js
+++ src/components/login/loginValidation.js
@@ -4,6 +4,7 @@ const loginSchema = yup
   .object({
     email: yup.string().required("Email required").email("Invalid email"),
     password: yup.string().required("Password required"),
+    otp: yup.string().length(6),
   })
   .required();
 
