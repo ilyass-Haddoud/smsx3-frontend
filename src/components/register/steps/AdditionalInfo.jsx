03c869de7bf3e5ae42d0d4eb5956b98c2f4d407e
ilyass <ilyass.haddoud@inetum.com>
2024-03-26 09:34:37 +0000

    refactoring ui for QR code

--- src/components/register/steps/AdditionalInfo.jsx
+++ src/components/register/steps/AdditionalInfo.jsx
@@ -1,23 +1,51 @@
 import { useForm } from "react-hook-form";
 import { setCredentials } from "../../../features/register/registerSlice";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { Fragment } from "react";
+import registerRequest from "../../../features/register/registerApi";
 
 const AdditionalInfo = ({ step, setStep }) => {
+  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiDialogContent-root": {
+      padding: theme.spacing(2),
+    },
+    "& .MuiDialogActions-root": {
+      padding: theme.spacing(1),
+    },
+  }));
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+    dispatch(registerRequest());
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setStep((step) => step + 1);
+  };
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   return (
     <div className="registerstep">
       <form
         onSubmit={handleSubmit((data) => {
           dispatch(setCredentials(data));
-          setStep((step) => step + 1);
+          setOpen(true);
         })}
       >
         <header>Sign Up</header>
@@ -58,6 +86,57 @@ const AdditionalInfo = ({ step, setStep }) => {
         </div>
         <button>Submit</button>
       </form>
+      <Fragment>
+        <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+        >
+          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+            Confirmation
+          </DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogContent dividers>
+            <Typography gutterBottom>
+              Thank you for providing your information. Your details have been
+              received successfully.
+            </Typography>
+            <Typography gutterBottom>
+              To enhance the security of your account, we will now generate a QR
+              code that you need to scan using your preferred authentication
+              app, such as Google Authenticator.
+            </Typography>
+            <Typography gutterBottom>
+              Once scanned, you will receive a one-time password (OTP) code.
+              This OTP code will be required every time you log in to your
+              account for added security.
+            </Typography>
+            <Typography gutterBottom>
+              Please proceed with submission. If there are any errors or changes
+              needed, you will be prompted to update the form accordingly.
+            </Typography>
+            <Typography gutterBottom>
+              Thank you for your cooperation.
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Ok
+            </Button>
+          </DialogActions>
+        </BootstrapDialog>
+      </Fragment>
     </div>
   );
 };
--- src/components/register/steps/OtpScanning.jsx
+++ src/components/register/steps/OtpScanning.jsx
@@ -1,18 +1,34 @@
 import { useDispatch, useSelector } from "react-redux";
 import registerRequest from "../../../features/register/registerApi";
+import { useNavigate } from "react-router-dom";
 
 const OtpScanning = () => {
   const state = useSelector((state) => state.registerReducer);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   console.log(state);
   return (
     <div className="otpscanning">
-      <h1>Scan the fucking code bellow</h1>
-      <button onClick={() => dispatch(registerRequest(state.supplier))}>
-        sign in
-      </button>
-      <p>{state.isLoading && "Loading"}</p>
+      <h2>Scan the code bellow</h2>
+      {!state.otpImage && (
+        <button
+          disabled={state.isLoading}
+          onClick={() => dispatch(registerRequest(state.supplier))}
+        >
+          {state.isLoading ? "Loading" : "Get QR Code"}
+        </button>
+      )}
+
       <p>{!state.isLoading && <img src={state.otpImage} />}</p>
+      {state.otpImage && (
+        <button
+          onClick={() => {
+            navigate("/auth/login");
+          }}
+        >
+          Done
+        </button>
+      )}
     </div>
   );
 };
