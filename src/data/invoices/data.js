3051b932891012a90a2d478afd4c423eda75492a
ilyass <ilyass.haddoud@inetum.com>
2024-04-03 15:40:34 +0000

    adding dataTable and mock invoices

---
+++ src/data/invoices/data.js
@@ -0,0 +1,71 @@
+const generateRandomInvoice = (id) => {
+  const sites = ["Site A", "Site B", "Site C"];
+  const types = ["Facture", "Avoir Facture Client"];
+  const etats = ["Validée", "En cours de traitement", "Non validée"];
+  const devises = ["EUR", "USD", "GBP"];
+  const typePaiements = ["Carte de crédit", "Virement bancaire", "Chèque"];
+
+  const randomIndex = (array) => Math.floor(Math.random() * array.length);
+
+  return {
+    id: id,
+    site_vente: sites[randomIndex(sites)],
+    type: types[randomIndex(types)],
+    numero_facture: `F2024${100 + id}`,
+    reference: `REF${100 + id}`,
+    date: `2024-04-${10 + id}`,
+    client_facture: `Client ${String.fromCharCode(65 + (id % 26))}`,
+    client_intitule: `Entreprise ${String.fromCharCode(
+      65 + (id % 26)
+    )}${String.fromCharCode(65 + (id % 26))}${String.fromCharCode(
+      65 + (id % 26)
+    )}`,
+    client_commande: `CMD00${id}`,
+    tiers_payeur: `Tiers ${String.fromCharCode(65 + (id % 26))}`,
+    client_groupe: `Groupe ${String.fromCharCode(65 + (id % 26))}`,
+    etat: etats[randomIndex(etats)],
+    devise: devises[randomIndex(devises)],
+    debut_echeance: `2024-05-${10 + id}`,
+    type_paiement: typePaiements[randomIndex(typePaiements)],
+    date_debut_periode: `2024-04-${10 + id}`,
+    date_fin_periode: `2024-04-${20 + id}`,
+    lignes: [
+      {
+        article: `Produit ${String.fromCharCode(65 + (id % 26))}`,
+        quantite: Math.floor(Math.random() * 10) + 1,
+        reste_a_livrer: Math.floor(Math.random() * 5),
+        prix_brut: Math.floor(Math.random() * 100) + 50,
+        remise_1: Math.floor(Math.random() * 10),
+        remise_2: Math.floor(Math.random() * 5),
+        frs1: Math.floor(Math.random() * 20),
+        total_ht: Math.floor(Math.random() * 1000) + 500,
+        taxe: Math.floor(Math.random() * 50),
+        total_ttc: Math.floor(Math.random() * 1200) + 1000,
+      },
+      {
+        article: `Produit ${String.fromCharCode(65 + ((id + 1) % 26))}`,
+        quantite: Math.floor(Math.random() * 10) + 1,
+        reste_a_livrer: Math.floor(Math.random() * 5),
+        prix_brut: Math.floor(Math.random() * 100) + 50,
+        remise_1: Math.floor(Math.random() * 10),
+        remise_2: Math.floor(Math.random() * 5),
+        frs1: Math.floor(Math.random() * 20),
+        total_ht: Math.floor(Math.random() * 1000) + 500,
+        taxe: Math.floor(Math.random() * 50),
+        total_ttc: Math.floor(Math.random() * 1200) + 1000,
+      },
+    ],
+    document: `Facture_F2024${100 + id}.pdf`,
+  };
+};
+
+const generateInvoices = (count) => {
+  const invoices = [];
+  for (let i = 1; i <= count; i++) {
+    invoices.push(generateRandomInvoice(i));
+  }
+  return invoices;
+};
+
+const data = generateInvoices(300);
+export default data;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,128 @@
+import React, { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Modal, Typography, Box, Button } from "@mui/material";
+import data from "../data/invoices/data";
+
 const Home = () => {
+  const [selectedInvoice, setSelectedInvoice] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleRowClick = (params) => {
+    setSelectedInvoice(params.row);
+    setOpenModal(true);
+  };
+
   return (
-    <div className="home">
-      <h1>Home Page</h1>
+    <div
+      style={{
+        height: 300,
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <DataGrid
+        rows={data}
+        columns={[
+          {
+            field: "numero_facture",
+            headerName: "Numéro de Facture",
+            flex: 1,
+          },
+          {
+            field: "client_facture",
+            headerName: "Client",
+            flex: 1,
+          },
+          { field: "etat", headerName: "État", flex: 1 },
+          { field: "date", headerName: "Date", flex: 1 },
+        ]}
+        rowsPerPageOptions={[5, 10, 20]}
+        onRowClick={handleRowClick}
+      />
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ width: 400, bgcolor: "background.paper", p: 2 }}>
+          <Typography variant="h6" gutterBottom>
+            Détails de la Facture
+          </Typography>
+          {selectedInvoice && (
+            <div>
+              <Typography variant="body1">
+                <strong>Numéro de Facture:</strong>
+                {selectedInvoice.numero_facture}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Site de vente:</strong> {selectedInvoice.site_vente}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Type:</strong> {selectedInvoice.type}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Référence:</strong> {selectedInvoice.reference}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Date:</strong> {selectedInvoice.date}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Client:</strong> {selectedInvoice.client_facture}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Intitulé du client:</strong>
+                {selectedInvoice.client_intitule}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Commande du client:</strong>
+                {selectedInvoice.client_commande}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Tiers payeur:</strong> {selectedInvoice.tiers_payeur}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Groupe client:</strong> {selectedInvoice.client_groupe}
+              </Typography>
+              <Typography variant="body1">
+                <strong>État:</strong> {selectedInvoice.etat}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Devise:</strong> {selectedInvoice.devise}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Début échéance:</strong>
+                {selectedInvoice.debut_echeance}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Type de paiement:</strong>
+                {selectedInvoice.type_paiement}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Date de début de période:</strong>
+                {selectedInvoice.date_debut_periode}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Date de fin de période:</strong>
+                {selectedInvoice.date_fin_periode}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Document:</strong> {selectedInvoice.document}
+              </Typography>
+            </div>
+          )}
+          <Button onClick={handleCloseModal}>Fermer</Button>
+        </Box>
+      </Modal>
     </div>
   );
 };
