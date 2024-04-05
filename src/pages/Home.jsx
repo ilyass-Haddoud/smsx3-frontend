f0074f9d25e246d072aad1ac1e8fd7fbd072d735
ilyass <ilyass.haddoud@inetum.com>
2024-04-05 15:24:01 +0000

    adding datagride

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,14 @@ import {
   GridActionsCellItem,
   GridRowEditStopReasons,
 } from "@mui/x-data-grid";
-import { Modal, Typography, Box, Button, Stack } from "@mui/material";
+import {
+  Modal,
+  Typography,
+  Box,
+  Button,
+  TextField,
+  Stack,
+} from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 import SaveIcon from "@mui/icons-material/Save";
@@ -17,6 +24,8 @@ const Home = () => {
   const [openModal, setOpenModal] = useState(false);
   const [rows, setRows] = useState(data);
   const [rowModesModel, setRowModesModel] = useState({});
+  const [editedInvoice, setEditedInvoice] = useState(null);
+  const [editedValues, setEditedValues] = useState({});
 
   const handleRowEditStop = (params, event) => {
     if (params.reason === GridRowEditStopReasons.rowFocusOut) {
@@ -24,28 +33,26 @@ const Home = () => {
     }
   };
 
-  const handleEditClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  const handleEditClick = (invoice) => () => {
+    setEditedInvoice(invoice);
+    setEditedValues(invoice.row);
+    setOpenModal(true);
   };
 
-  const handleSaveClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  const handleSaveClick = () => {
+    const updatedRows = rows.map((row) =>
+      row.id === editedInvoice.row.id ? editedValues : row
+    );
+    setRows(updatedRows);
+    setOpenModal(false);
   };
 
   const handleDeleteClick = (id) => () => {
     setRows(rows.filter((row) => row.id !== id));
   };
 
-  const handleCancelClick = (id) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-
-    const editedRow = rows.find((row) => row.id === id);
-    if (editedRow.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
-    }
+  const handleCancelClick = () => {
+    setOpenModal(false);
   };
 
   const processRowUpdate = (newRow) => {
@@ -57,13 +64,13 @@ const Home = () => {
   const handleRowModesModelChange = (newRowModesModel) => {
     setRowModesModel(newRowModesModel);
   };
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
 
-  const handleRowClick = (params) => {
-    setSelectedInvoice(params.row);
-    setOpenModal(true);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
   return (
@@ -76,157 +83,245 @@ const Home = () => {
         alignItems: "center",
       }}
     >
-      <Stack direction="column" spacing={2} height={"100%"} width={"100%"}>
-        <Button variant="contained">Ajouter une facture</Button>
-        <DataGrid
-          rows={rows}
-          columns={[
-            {
-              field: "numero_facture",
-              headerName: "Numéro de Facture",
-              flex: 1,
-              editable: true,
-            },
-            {
-              field: "client_facture",
-              headerName: "Client",
-              flex: 1,
-              editable: true,
-            },
-            { field: "etat", headerName: "État", flex: 1, editable: true },
-            { field: "date", headerName: "Date", flex: 1, editable: true },
-            {
-              field: "actions",
-              type: "actions",
-              headerName: "Actions",
-              cellClassName: "actions",
-              getActions: ({ id }) => {
-                const isInEditMode =
-                  rowModesModel[id]?.mode === GridRowModes.Edit;
+      <DataGrid
+        rows={rows}
+        columns={[
+          {
+            field: "numero_facture",
+            headerName: "Numéro de Facture",
+            flex: 1,
+            editable: true,
+          },
+          {
+            field: "client_facture",
+            headerName: "Client",
+            flex: 1,
+            editable: true,
+          },
+          { field: "etat", headerName: "État", flex: 1, editable: true },
+          { field: "date", headerName: "Date", flex: 1, editable: true },
+          {
+            field: "actions",
+            type: "actions",
+            headerName: "Actions",
+            cellClassName: "actions",
+            getActions: ({ id, ...invoice }) => {
+              const isInEditMode =
+                rowModesModel[id]?.mode === GridRowModes.Edit;
 
-                if (isInEditMode) {
-                  return [
-                    <GridActionsCellItem
-                      icon={<SaveIcon />}
-                      label="Save"
-                      sx={{
-                        color: "primary.main",
-                      }}
-                      onClick={handleSaveClick(id)}
-                    />,
-                    <GridActionsCellItem
-                      icon={<CancelIcon />}
-                      label="Cancel"
-                      className="textPrimary"
-                      onClick={handleCancelClick(id)}
-                      color="inherit"
-                    />,
-                  ];
-                }
-
+              if (isInEditMode) {
                 return [
                   <GridActionsCellItem
-                    icon={<EditIcon />}
-                    label="Edit"
-                    className="textPrimary"
-                    onClick={handleEditClick(id)}
-                    color="inherit"
+                    key="save"
+                    icon={<SaveIcon />}
+                    label="Save"
+                    sx={{
+                      color: "primary.main",
+                    }}
+                    onClick={handleSaveClick}
                   />,
                   <GridActionsCellItem
-                    icon={<DeleteIcon />}
-                    label="Delete"
-                    onClick={handleDeleteClick(id)}
+                    key="cancel"
+                    icon={<CancelIcon />}
+                    label="Cancel"
+                    className="textPrimary"
+                    onClick={handleCancelClick}
                     color="inherit"
                   />,
                 ];
-              },
+              }
+
+              return [
+                <GridActionsCellItem
+                  key="edit"
+                  icon={<EditIcon />}
+                  label="Edit"
+                  className="textPrimary"
+                  onClick={handleEditClick(invoice)}
+                  color="inherit"
+                />,
+                <GridActionsCellItem
+                  key="delete"
+                  icon={<DeleteIcon />}
+                  label="Delete"
+                  onClick={handleDeleteClick(id)}
+                  color="inherit"
+                />,
+              ];
             },
-          ]}
-          editMode="row"
-          rowModesModel={rowModesModel}
-          onRowModesModelChange={handleRowModesModelChange}
-          onRowEditStop={handleRowEditStop}
-          processRowUpdate={processRowUpdate}
-          rowsPerPageOptions={[10, 20, 30]}
-          onRowClick={handleRowClick}
-        />
-      </Stack>
+          },
+        ]}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        rowsPerPageOptions={[10, 20, 30]}
+      />
       <Modal
         open={openModal}
-        onClose={handleCloseModal}
+        onClose={handleCancelClick}
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <Box sx={{ width: 400, bgcolor: "background.paper", p: 2 }}>
+        <Box
+          sx={{
+            width: "80%",
+            maxWidth: 600,
+            bgcolor: "background.paper",
+            p: 2,
+          }}
+        >
           <Typography variant="h6" gutterBottom>
-            Détails de la Facture
+            Modifier la Facture
           </Typography>
-          {selectedInvoice && (
+          {editedInvoice && (
             <div>
-              <Typography variant="body1">
-                <strong>Numéro de Facture:</strong>
-                {selectedInvoice.numero_facture}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Site de vente:</strong> {selectedInvoice.site_vente}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Type:</strong> {selectedInvoice.type}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Référence:</strong> {selectedInvoice.reference}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Date:</strong> {selectedInvoice.date}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Client:</strong> {selectedInvoice.client_facture}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Intitulé du client:</strong>
-                {selectedInvoice.client_intitule}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Commande du client:</strong>
-                {selectedInvoice.client_commande}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Tiers payeur:</strong> {selectedInvoice.tiers_payeur}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Groupe client:</strong> {selectedInvoice.client_groupe}
-              </Typography>
-              <Typography variant="body1">
-                <strong>État:</strong> {selectedInvoice.etat}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Devise:</strong> {selectedInvoice.devise}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Début échéance:</strong>
-                {selectedInvoice.debut_echeance}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Type de paiement:</strong>
-                {selectedInvoice.type_paiement}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Date de début de période:</strong>
-                {selectedInvoice.date_debut_periode}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Date de fin de période:</strong>
-                {selectedInvoice.date_fin_periode}
-              </Typography>
-              <Typography variant="body1">
-                <strong>Document:</strong> {selectedInvoice.document}
-              </Typography>
+              <Stack direction={rows} spacing={10}>
+                <TextField
+                  label="Numéro de Facture"
+                  name="numero_facture"
+                  value={editedValues.numero_facture}
+                  fullWidth
+                  onChange={handleInputChange}
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  label="Site de vente"
+                  name="site_vente"
+                  value={editedValues.site_vente}
+                  fullWidth
+                  onChange={handleInputChange}
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  label="Type"
+                  name="type"
+                  value={editedValues.type}
+                  fullWidth
+                  onChange={handleInputChange}
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  label="Référence"
+                  name="reference"
+                  value={editedValues.reference}
+                  fullWidth
+                  onChange={handleInputChange}
+                  sx={{ mb: 2 }}
+                />
+              </Stack>
+              <TextField
+                label="Date"
+                name="date"
+                value={editedValues.date}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Client"
+                name="client_facture"
+                value={editedValues.client_facture}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Intitulé du client"
+                name="client_intitule"
+                value={editedValues.client_intitule}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Commande du client"
+                name="client_commande"
+                value={editedValues.client_commande}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Tiers payeur"
+                name="tiers_payeur"
+                value={editedValues.tiers_payeur}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Groupe client"
+                name="client_groupe"
+                value={editedValues.client_groupe}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="État"
+                name="etat"
+                value={editedValues.etat}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Devise"
+                name="devise"
+                value={editedValues.devise}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Début échéance"
+                name="debut_echeance"
+                value={editedValues.debut_echeance}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Type de paiement"
+                name="type_paiement"
+                value={editedValues.type_paiement}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Date de début de période"
+                name="date_debut_periode"
+                value={editedValues.date_debut_periode}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Date de fin de période"
+                name="date_fin_periode"
+                value={editedValues.date_fin_periode}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                label="Document"
+                name="document"
+                value={editedValues.document}
+                fullWidth
+                onChange={handleInputChange}
+                sx={{ mb: 2 }}
+              />
             </div>
           )}
-          <Button onClick={handleCloseModal}>Fermer</Button>
+          <Button onClick={handleSaveClick}>Modifier</Button>
+          <Button onClick={handleCancelClick}>Annuler</Button>
         </Box>
       </Modal>
     </div>
