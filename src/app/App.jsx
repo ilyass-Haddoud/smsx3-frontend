0871f1817f48a9671645da2cc53eeea312e8f20e
ilyass <ilyass.haddoud@inetum.com>
2024-04-09 08:34:25 +0000

    adding browser router with prema header component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,19 @@
-import { RouterProvider } from "react-router-dom";
-import router from "../router";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Login from "../components/login/Login";
+import Register from "../components/register/Register";
+import Home from "../pages/Home";
+import Header from "../components/shared/Header";
 
 const App = () => {
   return (
     <div className="app container">
-      <RouterProvider router={router} />
+      <Header />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Login />} path="auth/login" />
+        <Route element={<Register />} path="auth/register" />
+      </Routes>
       <ToastContainer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from "./app/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import Header from "./components/shared/Header";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Header />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
